b9b0de7bb5670e6c0130f3b63e0e80c5b2cf7b72
lanxeon <lanxeon@users.noreply.github.com>
2020-06-02 13:36:32 +0530

    improved screen loader

--- src/components/LoadingScreen/LoadingScreen.js
+++ src/components/LoadingScreen/LoadingScreen.js
@@ -1,13 +1,25 @@
-import React from 'react'
+// import React from 'react'
 
-import classes from './LoadingScreen.module.css';
+// import classes from './LoadingScreen.module.css';
+
+// export default function LoadingScreen(props) {
+//     return (
+//         <div className={classes.page}>
+//             <div className="loading">
+//                 Loading...
+//             </div>
+//         </div>
+//     );
+// }
+
+import React from "react";
+
+import classes from "./LoadingScreen.module.css";
 
 export default function LoadingScreen(props) {
-    return (
-        <div className={classes.page}>
-            <div className="loading">
-                Loading...
-            </div>
-        </div>
-    );
+	return (
+		<div className={classes.preloader}>
+			<div className={classes.loader}></div>
+		</div>
+	);
 }
--- src/containers/Content/UserFiles/File/ShareButton/ShareButton.js
+++ src/containers/Content/UserFiles/File/ShareButton/ShareButton.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
 
 import UserContext from "../../../../../context/user-context";
+import classes from "./ShareButton.module.css";
 
 class ShareButton extends Component {
 	static contextType = UserContext;
+
+	state = {
+		lol: "lol",
+	};
 	render() {
-		return <div></div>;
+		return (
+			<div className={classes.ShareButton}>
+				<button>Share</button>
+			</div>
+		);
 	}
 }
 
