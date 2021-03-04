8a044eb88388a5ce7a847926b0fd5292d82024b2
lanxeon <bswapnonil@gmail.com>
2021-03-04 10:07:52 +0530

    implemented file sharing with backend-blockchain connection

--- src/containers/Content/UserFiles/File/File.js
+++ src/containers/Content/UserFiles/File/File.js
@@ -5,97 +5,127 @@ import classes from "./File.module.css";
 import ShareButton from "./ShareButton/ShareButton";
 
 class File extends Component {
-	size = "";
-	reducedSize = null;
+  size = "";
+  reducedSize = null;
 
-	// componentWillMount = async () => {
-	// 	let fs = this.props.values.file.size;
-	// 	let i = 0;
-	// 	while (fs / 1024 > 1) {
-	// 		fs /= 1024;
-	// 		i += 1;
-	// 		this.size = i === 1 ? "KB" : i === 2 ? "MB" : i === 3 ? "GB" : "B";
-	// 		this.reducedSize = fs.toFixed(1);
-	// 	}
-	// };
+  // componentWillMount = async () => {
+  // 	let fs = this.props.values.file.size;
+  // 	let i = 0;
+  // 	while (fs / 1024 > 1) {
+  // 		fs /= 1024;
+  // 		i += 1;
+  // 		this.size = i === 1 ? "KB" : i === 2 ? "MB" : i === 3 ? "GB" : "B";
+  // 		this.reducedSize = fs.toFixed(1);
+  // 	}
+  // };
 
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		//for getting icon as per file extension
-		let ext = this.props.values.name.substr(this.props.values.name.lastIndexOf(".") + 1).toLowerCase();
-		this.img =
-			ext === "pdf"
-				? "picture_as_pdf"
-				: ext === "gif"
-				? "gif"
-				: ["html", "c", "cpp", "js", "css", "java", "py", "scss", ".sol", "cs", "ts"].includes(ext)
-				? "code"
-				: ["png", "jpg", "jpeg", "webp"].includes(ext)
-				? "insert_photo"
-				: ["mp4", "ogg", "mkv", "flv"].includes(ext)
-				? "movie"
-				: ["mp3", "3ga", "flac", "wav", "opus"].includes(ext)
-				? "music_note"
-				: "description";
+    //for getting icon as per file extension
+    let ext = this.props.values.name
+      ? this.props.values.name
+          .substr(this.props.values.name.lastIndexOf(".") + 1)
+          .toLowerCase()
+      : null;
 
-		//for formatting dates
-		this.date = new Date(this.props.values.dateAdded).toLocaleString();
+    this.img =
+      ext === "pdf"
+        ? "picture_as_pdf"
+        : ext === "gif"
+        ? "gif"
+        : [
+            "html",
+            "c",
+            "cpp",
+            "js",
+            "css",
+            "java",
+            "py",
+            "scss",
+            ".sol",
+            "cs",
+            "ts",
+          ].includes(ext)
+        ? "code"
+        : ["png", "jpg", "jpeg", "webp"].includes(ext)
+        ? "insert_photo"
+        : ["mp4", "ogg", "mkv", "flv"].includes(ext)
+        ? "movie"
+        : ["mp3", "3ga", "flac", "wav", "opus"].includes(ext)
+        ? "music_note"
+        : "description";
 
-		//for getting size in respective bytes
-		let fs = this.props.values.file.size;
-		let i = 0;
-		if (fs < 1024) {
-			this.size = "B";
-			this.reducedSize = fs;
-		} else {
-			while (fs / 1024 > 1) {
-				fs /= 1024;
-				i += 1;
-				this.size = i === 1 ? "KB" : i === 2 ? "MB" : i === 3 ? "GB" : "B";
-				this.reducedSize = fs.toFixed(1);
-			}
-		}
-	}
+    //for formatting dates
+    this.date = new Date(this.props.values.dateAdded).toLocaleString();
 
-	render() {
-		return (
-			<React.Fragment>
-				<div className={classes.file}>
-					<div className={classes.name}>
-						<div className={classes.fileContainer}>
-							<i className="material-icons">{this.img}</i>
-						</div>
-						<div className={classes.fileName}>
-							<span>{this.props.values.name}</span>
-						</div>
-					</div>
-					<div className={classes.size}>
-						<span>{this.reducedSize + this.size}</span>
-					</div>
-					<div className={classes.date}>
-						<span>{this.date}</span>
-					</div>
-					<div className={classes.download} style={{ transform: "translateY(-25%)" }}>
-						<button onClick={() => this.props.onDownload(this.props.values.file.path, this.props.values.name)}>
-							<div className={classes.iconContainer}>
-								<i className="material-icons">get_app</i>
-							</div>
-						</button>
-					</div>
-					<div className={classes.delete} style={{ transform: "translateY(-25%)" }}>
-						<button onClick={this.props.onDelete}>
-							<div className={classes.fileContainer}>
-								<i className="material-icons">delete</i>
-							</div>
-						</button>
-					</div>
-					<ShareButton values={this.props.values} />
-				</div>
-				<hr></hr>
-			</React.Fragment>
-		);
-	}
+    //for getting size in respective bytes
+    let fs = this.props.values.file.size;
+    let i = 0;
+    if (fs < 1024) {
+      this.size = "B";
+      this.reducedSize = fs;
+    } else {
+      while (fs / 1024 > 1) {
+        fs /= 1024;
+        i += 1;
+        this.size = i === 1 ? "KB" : i === 2 ? "MB" : i === 3 ? "GB" : "B";
+        this.reducedSize = fs.toFixed(1);
+      }
+    }
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className={classes.file}>
+          <div className={classes.name}>
+            <div className={classes.fileContainer}>
+              <i className="material-icons">{this.img}</i>
+            </div>
+            <div className={classes.fileName}>
+              <span>{this.props.values.name}</span>
+            </div>
+          </div>
+          <div className={classes.size}>
+            <span>{this.reducedSize + this.size}</span>
+          </div>
+          <div className={classes.date}>
+            <span>{this.date}</span>
+          </div>
+          <div
+            className={classes.download}
+            style={{ transform: "translateY(-25%)" }}
+          >
+            <button
+              onClick={() =>
+                this.props.onDownload(
+                  this.props.values.file.path,
+                  this.props.values.name
+                )
+              }
+            >
+              <div className={classes.iconContainer}>
+                <i className="material-icons">get_app</i>
+              </div>
+            </button>
+          </div>
+          <div
+            className={classes.delete}
+            style={{ transform: "translateY(-25%)" }}
+          >
+            <button onClick={this.props.onDelete}>
+              <div className={classes.fileContainer}>
+                <i className="material-icons">delete</i>
+              </div>
+            </button>
+          </div>
+          <ShareButton values={this.props.values} />
+        </div>
+        <hr></hr>
+      </React.Fragment>
+    );
+  }
 }
 
 export default File;
--- src/containers/Content/UserFiles/File/ShareButton/ShareButton.js
+++ src/containers/Content/UserFiles/File/ShareButton/ShareButton.js
@@ -5,172 +5,194 @@ import classes from "./ShareButton.module.css";
 import Axios from "axios";
 
 class ShareButton extends Component {
-	static contextType = UserContext;
+  static contextType = UserContext;
 
-	//refs are probably not needed
-	modal = React.createRef();
-	mainModal = React.createRef();
+  //refs are probably not needed
+  modal = React.createRef();
+  mainModal = React.createRef();
 
-	state = {
-		contract: this.context.contract,
-		sharing: false,
-		value: "",
-		formError: null,
-	};
+  state = {
+    contract: this.context.contract,
+    sharing: false,
+    value: "",
+    formError: null,
+  };
 
-	componentWillUnmount = () => {
-		document.removeEventListener("click", this.clickOutHandler);
-	};
+  componentWillUnmount = () => {
+    document.removeEventListener("click", this.clickOutHandler);
+  };
 
-	//it SOMEHOW works. dont ask
-	clickOutHandler = e => {
-		if (this.modal.current && !this.modal.current.contains(e.target)) {
-			this.setState({
-				sharing: false,
-			});
-		}
-		document.removeEventListener("click", this.clickOutHandler);
-	};
+  //it SOMEHOW works. dont ask
+  clickOutHandler = (e) => {
+    if (this.modal.current && !this.modal.current.contains(e.target)) {
+      this.setState({
+        sharing: false,
+      });
+    }
+    document.removeEventListener("click", this.clickOutHandler);
+  };
 
-	toggleSharing = () => {
-		let prevState = this.state.sharing;
+  toggleSharing = () => {
+    let prevState = this.state.sharing;
 
-		if (!prevState) {
-			this.setState({
-				sharing: !prevState,
-			});
-			document.addEventListener("click", this.clickOutHandler);
-		} else {
-			this.setState({
-				sharing: !prevState,
-			});
-			document.removeEventListener("click", this.clickOutHandler);
-		}
-	};
+    if (!prevState) {
+      this.setState({
+        sharing: !prevState,
+      });
+      document.addEventListener("click", this.clickOutHandler);
+    } else {
+      this.setState({
+        sharing: !prevState,
+      });
+      document.removeEventListener("click", this.clickOutHandler);
+    }
+  };
 
-	changedHandler = e => {
-		this.setState({
-			value: e.target.value,
-		});
-	};
+  changedHandler = (e) => {
+    this.setState({
+      value: e.target.value,
+    });
+  };
 
-	//for sharing a file. returns error for the various use cases
-	submitHandler = async e => {
-		e.preventDefault();
+  //for sharing a file. returns error for the various use cases
+  submitHandler = async (e) => {
+    e.preventDefault();
 
-		try {
-			let userExists = await Axios.get("http://localhost:3001/user/acquire/key/" + this.state.value);
+    try {
+      let userExists = await Axios.get(
+        "http://localhost:3001/user/acquire/key/" + this.state.value
+      );
 
-			let contractUpdateData = await this.state.contract.methods
-				.shareFile(this.props.values.file.hash, this.context.userPublicKey, userExists.data.key)
-				.send({ from: this.context.userPublicKey });
+      let contractUpdateData = await this.state.contract.methods
+        .shareFile(
+          this.props.values.file.hash,
+          this.context.userPublicKey,
+          userExists.data.key
+        )
+        .send({ from: this.context.userPublicKey });
 
-			if (contractUpdateData.events["FileShared"]) {
-				let fileCloudShare = await Axios.post("http://localhost:3001/cloud/share", {
-					from: this.context.userPublicKey,
-					to: this.state.value,
-					fileId: this.props.values.file._id,
-				});
+      if (contractUpdateData.events["FileShared"]) {
+        // let fileCloudShare = await Axios.post("http://localhost:3001/cloud/share", {
+        // 	from: this.context.userPublicKey,
+        // 	to: this.state.value,
+        // 	fileId: this.props.values.file._id,
+        // });
 
-				console.log(fileCloudShare);
-				alert("file shared to " + this.state.value);
-			}
+        let fileCloudShare = await Axios.post(
+          "http://localhost:3001/cloud/upload/dup",
+          {
+            hash: this.props.values.file.hash,
+            owner: userExists.data.key,
+            name: this.props.values.name,
+          }
+        );
 
-			this.setState({
-				formError: null,
-				value: "",
-				sharing: false,
-			});
-		} catch (err) {
-			this.setState({ formError: err.response.data.message });
-		}
-	};
+        console.log(fileCloudShare);
+        alert("file shared to " + this.state.value);
+      }
 
-	// render() {
-	// 	let errMsg = this.state.formError ? (
-	// 		<div className={classes.err}>
-	// 			<span>{this.state.formError}</span>
-	// 		</div>
-	// 	) : null;
+      this.setState({
+        formError: null,
+        value: "",
+        sharing: false,
+      });
+    } catch (err) {
+      this.setState({ formError: err.response.data.message });
+    }
+  };
 
-	// 	return (
-	// 		<div className={classes.ShareButton} ref={this.mainModal}>
-	// 			<button className={classes.btn} onClick={this.toggleSharing}>
-	// 				<span>Share</span>
-	// 			</button>
-	// 			{this.state.sharing ? (
-	// 				<div className={classes.formContainer} ref={this.modal}>
-	// 					<form className={classes.form} onSubmit={this.submitHandler}>
-	// 						<input
-	// 							className={classes.input}
-	// 							type="text"
-	// 							minLength="3"
-	// 							placeholder="Enter alias"
-	// 							maxLength="15"
-	// 							value={this.state.value}
-	// 							onChange={this.changedHandler}
-	// 						></input>
-	// 						{errMsg}
-	// 						<div className={classes.btnContainer}>
-	// 							<button
-	// 								style={{ border: "1px solid darkcyan", borderRadius: "10%", backgroundColor: "darkcyan" }}
-	// 							>
-	// 								<span>Share</span>
-	// 							</button>
-	// 							<button onClick={this.toggleSharing}>
-	// 								<span style={{ color: "red" }}>Cancel</span>
-	// 							</button>
-	// 						</div>
-	// 					</form>
-	// 				</div>
-	// 			) : null}
-	// 		</div>
-	// 	);
-	// }
+  // render() {
+  // 	let errMsg = this.state.formError ? (
+  // 		<div className={classes.err}>
+  // 			<span>{this.state.formError}</span>
+  // 		</div>
+  // 	) : null;
 
-	render() {
-		let errMsg = this.state.formError ? (
-			<div className={classes.err}>
-				<span>{this.state.formError}</span>
-			</div>
-		) : null;
+  // 	return (
+  // 		<div className={classes.ShareButton} ref={this.mainModal}>
+  // 			<button className={classes.btn} onClick={this.toggleSharing}>
+  // 				<span>Share</span>
+  // 			</button>
+  // 			{this.state.sharing ? (
+  // 				<div className={classes.formContainer} ref={this.modal}>
+  // 					<form className={classes.form} onSubmit={this.submitHandler}>
+  // 						<input
+  // 							className={classes.input}
+  // 							type="text"
+  // 							minLength="3"
+  // 							placeholder="Enter alias"
+  // 							maxLength="15"
+  // 							value={this.state.value}
+  // 							onChange={this.changedHandler}
+  // 						></input>
+  // 						{errMsg}
+  // 						<div className={classes.btnContainer}>
+  // 							<button
+  // 								style={{ border: "1px solid darkcyan", borderRadius: "10%", backgroundColor: "darkcyan" }}
+  // 							>
+  // 								<span>Share</span>
+  // 							</button>
+  // 							<button onClick={this.toggleSharing}>
+  // 								<span style={{ color: "red" }}>Cancel</span>
+  // 							</button>
+  // 						</div>
+  // 					</form>
+  // 				</div>
+  // 			) : null}
+  // 		</div>
+  // 	);
+  // }
 
-		return (
-			<div className={classes.ShareButton} ref={this.mainModal}>
-				<button className={classes.btn} onClick={this.toggleSharing}>
-					<div className={classes.shareContainer} style={{ transform: "translateY(-25%)" }}>
-						<i className="material-icons">supervisor_account</i>
-					</div>
-				</button>
-				{this.state.sharing ? (
-					<div className={classes.formContainer} ref={this.modal}>
-						<form className={classes.form} onSubmit={this.submitHandler}>
-							<input
-								className={classes.input}
-								type="text"
-								minLength="3"
-								placeholder="Enter alias"
-								maxLength="15"
-								value={this.state.value}
-								onChange={this.changedHandler}
-							></input>
-							{errMsg}
-							<div className={classes.btnContainer}>
-								<button
-									style={{ border: "1px solid darkcyan", borderRadius: "10%", backgroundColor: "darkcyan" }}
-								>
-									<span>Share</span>
-								</button>
-								<button onClick={this.toggleSharing}>
-									<span style={{ color: "red" }}>Cancel</span>
-								</button>
-							</div>
-						</form>
-					</div>
-				) : null}
-			</div>
-		);
-	}
+  render() {
+    let errMsg = this.state.formError ? (
+      <div className={classes.err}>
+        <span>{this.state.formError}</span>
+      </div>
+    ) : null;
+
+    return (
+      <div className={classes.ShareButton} ref={this.mainModal}>
+        <button className={classes.btn} onClick={this.toggleSharing}>
+          <div
+            className={classes.shareContainer}
+            style={{ transform: "translateY(-25%)" }}
+          >
+            <i className="material-icons">supervisor_account</i>
+          </div>
+        </button>
+        {this.state.sharing ? (
+          <div className={classes.formContainer} ref={this.modal}>
+            <form className={classes.form} onSubmit={this.submitHandler}>
+              <input
+                className={classes.input}
+                type="text"
+                minLength="3"
+                placeholder="Enter alias"
+                maxLength="15"
+                value={this.state.value}
+                onChange={this.changedHandler}
+              ></input>
+              {errMsg}
+              <div className={classes.btnContainer}>
+                <button
+                  style={{
+                    border: "1px solid darkcyan",
+                    borderRadius: "10%",
+                    backgroundColor: "darkcyan",
+                  }}
+                >
+                  <span>Share</span>
+                </button>
+                <button onClick={this.toggleSharing}>
+                  <span style={{ color: "red" }}>Cancel</span>
+                </button>
+              </div>
+            </form>
+          </div>
+        ) : null}
+      </div>
+    );
+  }
 }
 
 export default ShareButton;
